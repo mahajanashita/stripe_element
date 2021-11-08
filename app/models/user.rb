93ff223eab6574313f3dcf73c9cafd1e22eba5c0
Ashita Mahajan <ashitamahajandal@gmail.com>
2021-11-08 15:34:53 -0800

    Added Stripe customer id

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  def to_s
+  	email
+  end
+
+  after_create do
+     customer = Stripe::Customer.create(email: self.email)
+     update(stripe_customer_id: customer.id)  	 #COMMENT THIS FOR WEBHOOKS
+  end
+  #if self.stripe_customer_id.nil?
+              #customer = Stripe::Customer.create(email: self.email)
+              #here we are creating a stripe customer with the help of the Stripe library and pass as parameter email. 
+              #update(stripe_customer_id: customer.id)
+              #we are updating current_user and giving to it stripe_id which is equal to id of customer on Stripe
+            #end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_08_230056) do
+ActiveRecord::Schema.define(version: 2021_11_08_232750) do
 
   create_table "installs", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -42,6 +42,9 @@ ActiveRecord::Schema.define(version: 2021_11_08_230056) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "plan"
+    t.string "subscription_status", default: "incomplete"
+    t.string "stripe_customer_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
